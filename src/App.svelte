c7b0ecf81072bfbc9a01a727db5ea8da4821fdde
Veronica isla <veroislagleaniz@gmail.com>
2022-07-01 11:48:55 +0200

    moved folders

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,157 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+import Header from "./components/Header.svelte";
+import PreInfo from "./components/PreInfo.svelte";
+import Filters from "./components/Filters.svelte";
+import ListProducts from "./components/ListProducts.svelte";
+import { onMount } from 'svelte';
+
+let products = []
+// ---> Unique me da los checkbox sin repetir
+let uniqueDepartment = [];
+let uniqueBrand = []; 
+// ---> checkbox seleccionados
+let checkboxBrand = [];
+let checkboxDepartment = [];
+
+  $: console.log(checkboxBrand);
+  $: console.log(checkboxDepartment)
+
+//FETCH
+    onMount(async () => {
+		const res = await fetch(`https://5f86d366c8a16a0016e6bcd4.mockapi.io/products`);
+		products= await res.json();  
+    getDepartment();
+    getBrand();
+    // console.log(products);
+	});
+
+
+// FETCH, Hago el fetch fuera de onMount por ver si eso es el problema, si que se pintan los productos, pero el console.log(products) sigue sin devolverme nada, creo que por eso no puedo hacer el filtrado, porque la variable products fuera del fetch no tiene ningún valor, por lo que al pasarle a mi componente <ListProducts products={productFiltered}/> realmente no le estoy pasando nada.
+
+//  fetch (`https://5f86d366c8a16a0016e6bcd4.mockapi.io/products`)
+// .then(response => response.json())
+// .then(data => products = data)
+// console.log(products); 
+
+
+//GET CHECKKBOX DEPARTMENT
+    const getDepartment = () => {
+    const productDepartment = products.map((product) => product.departmentLabel);
+    //  console.log(productDepartment);
+    uniqueDepartment = productDepartment.filter((department,index) => {
+    return productDepartment.indexOf(department) === index;
+    });
+    uniqueDepartment = uniqueDepartment.sort()
+  };
+
+//GET CHECKKBOX BRAND
+    const getBrand = () => {
+    const productBrand = products.map((product) => product.brand.label);
+    // console.log(productBrand);
+    uniqueBrand = productBrand.filter((brand,index) => {
+    return productBrand.indexOf(brand) === index;
+    }) 
+    uniqueBrand = uniqueBrand.sort();
+  } 
+  // onMount(() => getBrand()) ----> Me gustaría hacer el onMount aquí, y no cuando hago el fetch, pero desde aquí no funciona. 
+
+ 
+//FILTERS
+  const handlecheckboxBrand = (value) => {
+    if (checkboxBrand.includes(value)) {
+      const newBrand = checkboxBrand.filter((item) => item !== value);
+      checkboxBrand=(newBrand);
+    } else {
+      checkboxBrand=([...checkboxBrand, value]);
+    }};
+
+    const handlecheckboxDepartment = (value) => {
+    if (checkboxDepartment.includes(value)) {
+      const newDepartment = checkboxDepartment.filter((item) => item !== value);
+      checkboxDepartment=(newDepartment);
+    } else {
+      checkboxDepartment=([...checkboxDepartment, value]);
+    }};
+
+
+// LISTADO PRODUCTOS FILTRADOS
+$: productFiltered = 
+  products
+  .filter((item) => {
+    if (checkboxBrand.length === 0) {
+      return true;
+      
+    } else {
+      return checkboxBrand.includes(item.brand.label)
+    }})
+
+    .filter((item) => {
+    if (checkboxDepartment.length === 0) {
+      return true;
+    } else {
+      return checkboxDepartment.includes(item.departmentLabel)
+    }
+    }); 
+    
+
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
+<link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
 
-  <Counter />
 
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
+ <section lang="less">
+ <Header/> 
+ <PreInfo/>
+ <div class="computerVersion">
+  <Filters {uniqueBrand}  {uniqueDepartment} {handlecheckboxBrand} {handlecheckboxDepartment}/>
+  <!-- <ListProducts products={products}/>   -->
+  <ListProducts products={productFiltered}/> 
+ <!--  A ListProducts le pasaría como props en vez de la variable {products}, la variable con los filtros {productFiltered}--->
+</div>
+</section> 
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
-</main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
+ 
+<style lang="less">
+  @import './styles/core/Reset.less';
+  @import './styles/core/Vars.less';
+  @import './styles/components/App.less';
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- //FilterByDepartment -->
+ <!--1. productDepartment me de vuelve un nuevo listado con solo el tipo de deporte ---->
+ <!-- 2. uniqueDepartment me devuelve listado sin que se repita el tipo de deporte -->
+  <!--  3. indexOf, dime la posición actual de cada deporte -->
+  <!-- De todas los deportes, que las he guardado en el array productDepartment, voy a filtrar, por dos parametros, el department y el index (posicion en la que se encuentra ese elemento.) del array de productdepartment dime cual es la posicion (indexOf) de la ciudad en la que estoy parada. Si estoy en el deporte volley y su posicion es 1, y el indice en el que estoy parado es 1 entonces devuelmela.Si vuelve a aparecer volley en una posicion difernete que no es el 1, entonces no me lo devuelve  -->
+  <!-- //--- Otra manera de hacerlo, internamente el new SET me genera un objeto de valores únicos.
+    // const uniqueDepartment = new Set(productDepartment);
+    // const uniques = [...uniqueDepartment]
+    // console.log(uniques); -->
--- src/lib/Counter.svelte
+++
@@ -1,9 +0,0 @@
-<script>
- 
-</script>
-
-<p>prueba</p>
-
-<style>
-
-</style>
